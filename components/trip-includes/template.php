65b7bf357663d732b85df82fc26df5fdfdda1858
Your Name <your_email@address>
2026-04-27 15:31:32 +0100

    updates to left align and responsive padding

--- components/trip-includes/template.php
+++ components/trip-includes/template.php
@@ -5,7 +5,7 @@ use Gust\SVG;
 ?>
 
 <section id="trip-includes" class="<?= classes('trip-includes', 'wp-block', $this->classes) ?>" <?= attributes($this->attributes) ?>>
-    <div class="trip-includes__inner content-width-sm">
+    <div class="trip-includes__inner content-width-sm align-left">
         <?= \Gust\Components\Heading::make(
             el: 'h4',
             heading: __("What's included", 'gust'),
